85edcae776119bf80be67ac465eae2bd5d0f8bb7
Ilhom Dadadjanov <idadadjanov@gmail.com>
2022-10-29 14:40:18 +0200

    HTML & CSS Done

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-let form;
+let form;               //Get whole form element 
+let cvcNums;            //CVC number reference
+let logoImg;            //Logotype of the company
+
+
+let cardNumEl;         //Card numbers placeholder
+
+
+
+
+/* 
 let warnTextEl2;
 let warnTextEl1;
-let logoImg;
+
 let numbers;
 let confirmBtn;
-let continueBtn;
+let continueBtn; */
+
+let test;
 //---------------------------------------
 function init() {
-    form = document.getElementById("form");
-    form.cvc.addEventListener("keyup", checkCvc);
+    /* form = document.getElementById("form");
+    form.cvc.addEventListener("keyup", numberCVC); */
+
+    logoImg = document.getElementById("logoImg");
+
+
+    cardNumEl = document.querySelectorAll(".nums");
+
+
+    test = document.getElementById("text");
+    test.addEventListener("keyup", cardControl);
+
+
+
+
+    /* 
     form.year.addEventListener("keyup", checkYear);
     form.month.addEventListener("keyup", checkMonth);
     form.fullName.addEventListener("keyup", checkName);
@@ -23,68 +49,143 @@ function init() {
     confirmBtn.addEventListener("click", greating);
 
     continueBtn = document.getElementById("continue");
-    continueBtn.addEventListener("click", thanks);
+    continueBtn.addEventListener("click", thanks); */
 }
 window.addEventListener("load", init);
 //End init
 
-//Confirm function
-function greating() {
-    let inputs = form.querySelectorAll("input");
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value == 0) {
-            inputs[i].style.borderColor = "red";
+//------------------------------------------------------------
+//Control of CVC number
+/* function numberCVC() {
+    let regex = /(\d{2,3})\d+/g;
+    let testPattern = regex.test(this.value);
+    let changeValue = this.value.slice(0, -1);
+    let arr = this.value.split("");
+    this.style.textTransform = "uppercase";
+
+    if (arr.length == 4) {
+        this.value = changeValue;
+        this.style.borderColor = "grey";
+    } else if (arr.length > 2) {
+        if (!testPattern) {
+            this.style.borderColor = "#ff66cc";
         } else {
-            form.parentNode.classList.add("greating");
+            this.style.borderColor = "grey";
         }
     }
+    cvcNums.innerHTML = this.value;
+} */
+
+//------------------------------------------------------------
+//Control of card number
+function cardControl() {
+
+
+    /* 
+    let regex = /(\d{15,16})+/g;
+    let arr = [];
+    arr = this.value.split(""); 
+    */
+
+    /*
+    if (arr.length >= 16) {
+         let changeValue = this.value.slice(0, -1);
+         let newArr = arr.slice(0, -1);
+         this.value = changeValue;
+         if (!regex.test(this.value)) {
+             console.log("Style red");
+             this.style.borderColor = "#ff66cc";
+         } else {
+             console.log("Style green");
+             this.style.borderColor = "grey";
+         }
+     } else if (arr.length == 17) {
+         let changeValue = this.value.slice(0, -1);
+         let newArr = arr.slice(0, -1);
+         this.value = changeValue;
+         if (!regex.test(this.value)) {
+             console.log("Style red");
+             this.style.borderColor = "#ff66cc";
+         } else {
+             console.log("Style green");
+             this.style.borderColor = "grey";
+         }
+     }
+ 
+     for (let i = 0; i < arr.length; i++) {
+         if (cardNumEl[i]) {
+             cardNumEl[i].innerHTML = arr[i];
+         }
+    } 
+    */
+}
+
+
+
+
+
+
+
+
+
+
+//Confirm function
+function greating() {
+    /*  let inputs = form.querySelectorAll("input");
+     for (let i = 0; i < inputs.length; i++) {
+         if (inputs[i].value == 0) {
+             inputs[i].style.borderColor = "red";
+         } else {
+             form.parentNode.classList.add("greating");
+         }
+     } */
 }
 //hide thanks
 function thanks() {
-    form.parentNode.classList.remove("greating");
+    /* form.parentNode.classList.remove("greating"); */
 }
 //Check Number of card
 function cardNumber() {
-    let regex = /^(\d{0,16})\d+$/gi;
-    if (!regex.test(this.value)) {
-        warnTextEl1.classList.add("warn");
-    } else {
-        warnTextEl1.classList.remove("warn");
-    }
-
-    let numberToField = this.value;
-    let arr = this.value.split("");
-
-    for (let i = 0; i < arr.length; i++) {
-        if (numbers[i]) {
-            numbers[i].innerHTML = arr[i];
+    /*     let regex = /^(\d{0,16})\d+$/gi;
+        if (!regex.test(this.value)) {
+            warnTextEl1.classList.add("warn");
+        } else {
+            warnTextEl1.classList.remove("warn");
         }
-    }
-
-    if (arr.length == 17) {
-        this.value = numberToField.slice(0, -1);
-    }
+    
+        let numberToField = this.value;
+        let arr = this.value.split("");
+    
+        for (let i = 0; i < arr.length; i++) {
+            if (numbers[i]) {
+                numbers[i].innerHTML = arr[i];
+            }
+        }
+    
+        if (arr.length == 17) {
+            this.value = numberToField.slice(0, -1);
+        } */
 }
 
 //Check month field conteins only number
 function checkMonth() {
-    let expireDate = document
-        .getElementById("expireDate")
-        .querySelectorAll("p");
-    let date = Number(this.value);
-
-    if (date > 12) {
-        this.style.backgroundColor = "";
-        warnTextEl2.classList.add("warn");
-    } else if (date < 10) {
-        warnTextEl2.classList.remove("warn");
-        expireDate[0].innerHTML = this.value + " /";
-    }
+    /*  let expireDate = document
+         .getElementById("expireDate")
+         .querySelectorAll("p");
+     let date = Number(this.value);
+ 
+     if (date > 12) {
+         this.style.backgroundColor = "";
+         warnTextEl2.classList.add("warn");
+     } else if (date < 10) {
+         warnTextEl2.classList.remove("warn");
+         expireDate[0].innerHTML = this.value + " /";
+     } */
 }
 
 //Check year field conteins only number
 function checkYear() {
-    let expireDate = document
+    /* let expireDate = document
         .getElementById("expireDate")
         .querySelectorAll("p");
     let regMonth = /^[0-9]{1,2}$/;
@@ -94,12 +195,12 @@ function checkYear() {
         this.style.backgroundColor = "";
         warnTextEl2.classList.add("warn");
     }
-    expireDate[1].innerHTML = this.value;
+    expireDate[1].innerHTML = this.value; */
 }
 
 //Check CVC field conteins only number
 function checkCvc() {
-    let cvcNum = document.getElementById("csvNumber");
+    /* let cvcNum = document.getElementById("csvNumber");
     let regCvc = /^[0-9]{3}$/;
     if (regCvc.test(this.value)) {
         warnTextEl2.classList.remove("warn");
@@ -108,12 +209,12 @@ function checkCvc() {
         warnTextEl2.classList.add("warn");
     }
 
-    cvcNum.innerHTML = this.value;
+    cvcNum.innerHTML = this.value; */
 }
 
 //Check Name field conteins only number
 function checkName() {
-    let nameElem = document.getElementById("holderName");
+    /* let nameElem = document.getElementById("holderName");
     this.style.textTransform = "capitalize";
-    nameElem.innerHTML = this.value;
+    nameElem.innerHTML = this.value; */
 }
