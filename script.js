f5d872b1a163818670d6c43492dcd0aa5d0f5d72
Ilhom Dadadjanov <idadadjanov@gmail.com>
2022-10-29 21:27:22 +0200

    small changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let formField;
 let dateEl;
 let spanEl;
+let numSpanEl;
 /* Initialize web page before loading JavaScript */
 function init() {
     formField = document.getElementById("formField");
@@ -9,9 +10,12 @@ function init() {
     formField.expMonth.addEventListener("keyup", focusOnMonth);
     formField.expYear.addEventListener("keyup", focusOnYear);
     formField.cvcNum.addEventListener("keyup", focusOnCvc);
+    formField.cardNumber.addEventListener("keyup", checkCardNumber);
+    formField.submit.addEventListener("click", submutForm);
 
     dateEl = document.querySelectorAll(".hero-front-info_month span");
     spanEl = document.getElementById("warningDate");
+    numSpanEl = document.getElementById("warningNumber");
 
 } //End init
 window.addEventListener("load", init);
@@ -94,3 +98,71 @@ function focusOnCvc() {
         }
     }
 } //End focusOnCvc
+
+/* Focus on Card Number Field */
+function checkCardNumber() {
+    let spanEl = document.getElementById("warningNumber");
+    let arr = this.value.split("");
+
+    let newValue = this.value.slice(0, -1);
+    if (arr.length > 16) {
+        this.value = newValue;
+    }
+
+    let regex = /\d{16}/gi;
+    if (arr.length > 15) {
+        if (!regex.test(this.value)) {
+            this.style.borderColor = "red";
+            numSpanEl.innerHTML = "Only numbers";
+            numSpanEl.style.opacity = 1;
+        } else {
+            this.style.borderColor = "#8e8593";
+            numSpanEl.innerHTML = "&nbsp;"
+            numSpanEl.style.opacity = 0;
+        }
+    }
+    /* 
+    if (arr.length <= 16) {
+        if (!regex.test(this.value)) {
+            this.style.borderColor = "red";
+            numSpanEl.innerHTML = "Only numbers";
+            numSpanEl.style.opacity = 1;
+        } else {
+            this.style.borderColor = "#8e8593";
+            numSpanEl.innerHTML = "&nbsp;"
+            numSpanEl.style.opacity = 0;
+        }
+        this.value = newValue;
+    } */
+
+    let numberEl = document.querySelectorAll(".hero-front-numbers-part_number");
+    for (let i = 0; i < arr.length; i = i + 3) {
+        if (arr.length >= 4) {
+            numberEl[0].innerHTML = arr[0] + arr[1] + arr[2] + arr[3]
+        }
+        if (arr.length >= 8) {
+            numberEl[1].innerHTML = arr[4] + arr[5] + arr[6] + arr[7]
+        }
+        if (arr.length >= 12) {
+            numberEl[2].innerHTML = arr[8] + arr[9] + arr[10] + arr[11]
+        }
+        if (arr.length >= 16) {
+            numberEl[3].innerHTML = arr[12] + arr[13] + arr[14] + arr[15]
+        }
+    }
+} //End checkCardNumber
+
+/* Submit form function */
+function submutForm(e) {
+    e.preventDefault();
+    let fields = formField.getElementsByTagName("input");
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i].value == "") {
+            fields[i].style.borderColor = "red";
+            return;
+        }
+    }
+
+
+
+}
